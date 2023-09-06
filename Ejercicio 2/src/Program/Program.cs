f15ac575c018e80da41fb6f03d0095d33d93df19
Diego Moreno <diego.morenoa@correo.ucu.edu.uy>
2023-09-06 01:30:53 -0300

    Todo terminado

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -8,6 +8,8 @@ namespace Program
         static void Main(string[] args)
         {
             if (AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "1.212.121-2", 19, "Carlos Dominica", "Gynecologist", "1.234.567-2" , "Wall Street", DateTime.Now))
+            // Si todo es valido entonces la función CreateAppointment devolverá True. En dicho caso creamos el paciente, el doctor y la cita.
+            // Si cualquier información no es válida entonces no creamos ningún objeto y se imprimirá por qué y que no se pudo procesar ni agendar.
             {
                 Patient Steven = new Patient("Steven Jhonson", "986782342", "1.212.121-2", 19);
                 Doctor Carlos = new Doctor("Carlos Dominica", "Gynecologist", "1.234.567-2");
@@ -16,7 +18,7 @@ namespace Program
             }
             else
             {
-                Console.WriteLine("\nAppointment could not be scheduled. Please correct the missing parts indicated above.")
+                Console.WriteLine("\nAppointment could not be scheduled. Please correct the missing parts indicated above."); // Si sale false se imprime eso.
             }
         }
     }
